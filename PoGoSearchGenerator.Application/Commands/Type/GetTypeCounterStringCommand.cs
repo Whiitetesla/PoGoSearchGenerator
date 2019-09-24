13b13abacfbfb1fdd843a8fdb687440b940cc2e0
Niels Hviid <whiite.h.l@gmail.com>
2019-09-24 10:32:08 +0200

    added crude form of string generator

--- PoGoSearchGenerator.Application/Commands/Type/GetTypeCounterStringCommand.cs
+++ PoGoSearchGenerator.Application/Commands/Type/GetTypeCounterStringCommand.cs
@@ -57,69 +57,171 @@ namespace PoGoSearchGenerator.Application.Commands.Type
                 .Where(x => request.TypeConter.Types.Contains(x.Type))
                 .ToList();
 
+            //create list of all types that is related to our types
             var damageScores = new List<DamageRelationScore>();
+
+            //create the scores we need to add to the types
             var double_damage_from_score = 1.6;
             var half_damage_from_score = 0.6;
             var no_damage_from_score = 0;
 
+            //loops the damageRelations to get all the types and add there scores to them
             foreach (var damageRelation in damageRelations)
             {
                 foreach (var obj in damageRelation.Double_damage_from)
                 {
-                    if (damageScores.Any(x => x.Name == obj.Types.Name))
-                        damageScores.FirstOrDefault(x => x.Name == obj.Types.Name).Score *= double_damage_from_score;
+                    var type = _context.Set<Types>().Find(obj.TypesId);
+                    if (type == null)
+                        continue;
+
+                    if (damageScores.Any(x => x.Name == type.Name))
+                        damageScores.FirstOrDefault(x => x.Name == type.Name).Score *= double_damage_from_score;
 
                     else
                         damageScores.Add(new DamageRelationScore()
                         {
-                            Name = obj.Types.Name,
+                            Name = type.Name,
                             Score = double_damage_from_score
                         });
                 }
 
                 foreach (var obj in damageRelation.Half_damage_from)
                 {
-                    if (damageScores.Any(x => x.Name == obj.Types.Name))
-                        damageScores.FirstOrDefault(x => x.Name == obj.Types.Name).Score *= half_damage_from_score;
+                    var type = _context.Set<Types>().Find(obj.TypesId);
+                    if (type == null)
+                        continue;
+
+                    if (damageScores.Any(x => x.Name == type.Name))
+                        damageScores.FirstOrDefault(x => x.Name == type.Name).Score *= half_damage_from_score;
 
                     else
                         damageScores.Add(new DamageRelationScore()
                         {
-                            Name = obj.Types.Name,
+                            Name = type.Name,
                             Score = half_damage_from_score
                         });
                 }
 
                 foreach (var obj in damageRelation.No_damage_from)
                 {
-                    if (damageScores.Any(x => x.Name == obj.Types.Name))
-                        damageScores.FirstOrDefault(x => x.Name == obj.Types.Name).Score *= no_damage_from_score;
+                    var type = _context.Set<Types>().Find(obj.TypesId);
+                    if (type == null)
+                        continue;
+
+                    if (damageScores.Any(x => x.Name == type.Name))
+                        damageScores.FirstOrDefault(x => x.Name == type.Name).Score *= no_damage_from_score;
 
                     else
                         damageScores.Add(new DamageRelationScore()
                         {
-                            Name = obj.Types.Name,
+                            Name = type.Name,
                             Score = no_damage_from_score
                         });
                 }
 
             }
 
+            //create string to hold the information we need to return
             var returnStr = "";
 
-            if (request.TypeConter.Stab)
-            {
-                returnStr += damageScores.First(x => x.Score > 1.5).Name;
+            //the minimum score has to be to count as "super effective"
+            var powerMin = 1.5;
 
-                foreach (var type in damageScores)
+            //change the minimum score to only coun't double "super effectives"
+            if (request.TypeConter.DoubleSuper)
+                powerMin = 2.5;
+
+            //get the first type from the list that has a score more than our powerMin
+            var firstSuper = damageScores.FirstOrDefault(x => x.Score > powerMin);
+
+            //check to see if we have any "super effective" moves to work with 
+            //and only weakness isn't true
+            if (firstSuper != null && !request.TypeConter.OnlyWeakness)
+            {
+                //we there is requested for STAP we add the types
+                if (request.TypeConter.Stab)
                 {
-                    if (type.Score > 1.5 && type != damageScores.First(x => x.Score > 1.5))
+                    returnStr += firstSuper.Name;
+
+                    foreach (var type in damageScores.Where(x => x.Score > powerMin && x.Name != firstSuper.Name))
+                    {
                         returnStr += $",{type.Name}";
+                    }
+
+                    returnStr += "&";
+                }
+
+                //setup for all fast attacks
+                returnStr += $"@1{firstSuper.Name}";
+
+                foreach (var type in damageScores.Where(x => x.Score > powerMin && x.Name != firstSuper.Name))
+                {
+                    returnStr += $",@1{type.Name}";
+                }
+
+                //use the AttackRelation to determan if we need to use & or ,
+                if (request.TypeConter.AttackRelation)
+                    returnStr += "&";
+                else
+                    returnStr += ",";
+
+                //setup for charged and extra moves
+                returnStr += $"@2{firstSuper.Name},@3{firstSuper.Name}";
+
+                foreach (var type in damageScores.Where(x => x.Score > powerMin && x.Name != firstSuper.Name))
+                {
+                    returnStr += $",@2{type.Name},@3{type.Name}";
+                }
+
+                returnStr += "&";
+            }
+
+            //add cp and hp limit
+            returnStr += $"cp{request.TypeConter.CombatPoints}-";
+            returnStr += $"&hp{request.TypeConter.HitPoints}-";
+
+            //check to see if we need to add a removal for types that is weak to the type attack
+            if (request.TypeConter.WeaknessType)
+            {
+                var typeNames = new List<string>();
+
+                //loop damageRelation to get all types its strong against
+                foreach (var damageRelation in damageRelations)
+                {
+                    foreach (var typeRelation in damageRelation.Double_damage_to)
+                    {
+                        var type = _context.Set<Types>().Find(typeRelation.TypesId);
+                        if (type == null)
+                            continue;
+
+                        if (!typeNames.Any(x => x == type.Name))
+                            typeNames.Add(type.Name);
+                    }
+                }
+
+                //adds all the types that is weak to the list
+                foreach (var typeName in typeNames)
+                {
+                    //adds not operation to the list for removal of the types
+                    returnStr += $"&!{typeName}";
                 }
             }
 
-            //add of same type together in class prop
-            //return all with damage number above 1.5
+            if (request.TypeConter.WeaknessMove)
+            {
+                var firstWeak = damageScores.FirstOrDefault(x => x.Score < 0.7);
+
+                if(firstWeak != null)
+                {
+                    //setup for all fast attacks
+                    returnStr += $"&!@1{firstWeak.Name}&!@2{firstWeak.Name}&!@3{firstWeak.Name}";
+
+                    foreach (var type in damageScores.Where(x => x.Score < 0.7 && x.Name != firstWeak.Name))
+                    {
+                        returnStr += $"&!@1{type.Name}&!@2{type.Name}&!@3{type.Name}";
+                    }
+                }
+            }
 
             return returnStr;
         }
--- PoGoSearchGenerator.Domain/Dto/TypeCounterDto.cs
+++ PoGoSearchGenerator.Domain/Dto/TypeCounterDto.cs
@@ -32,9 +32,25 @@ namespace PoGoSearchGenerator.Domain.Dto
         public bool WeaknessType { get; set; }
 
         /// <summary>
+        /// should we add moves that are weak to the type you are against
+        /// </summary>
+        public bool WeaknessMove { get; set; }
+
+        /// <summary>
         /// the relation between fast and charged
         /// if it is an & (and) or ,(or)
         /// </summary>
-        public bool  AttackRelation { get; set; }
+        public bool AttackRelation { get; set; }
+
+        /// <summary>
+        /// if we only have to take the ones that are double super agains the type combo
+        /// this is only usefull if there are move than 1 type
+        /// </summary>
+        public bool DoubleSuper { get; set; }
+
+        /// <summary>
+        /// if we only want all that we should't take 
+        /// </summary>
+        public bool OnlyWeakness { get; set; }
     }
 }
